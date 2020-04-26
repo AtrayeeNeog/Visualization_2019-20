d11352745b63bd31b008e821ba6ff5e94fdb8054
AtrayeeNeog <44379864+AtrayeeNeog@users.noreply.github.com>
2020-04-26 14:59:21 +0200

    Update ParallelCoordinates.R

--- RCodes_Atrayee/ParallelCoordinates.R
+++ RCodes_Atrayee/ParallelCoordinates.R
@@ -2,6 +2,8 @@ library(GGally)
 
 ncol(dt)
 
-ggparcoord(dt, columns = 1: ncol(dt), groupColumn = 4)
-ggparcoord(qt1, columns = 1: ncol(qt1), groupColumn = 4)
-ggparcoord(qt2, columns = 1: ncol(qt2), groupColumn = 4)
+ggparcoord(dt, columns = 1: ncol(dt), groupColumn = 4) # Parallel Coords for the Template
+ggparcoord(qt1, columns = 1: ncol(qt1), groupColumn = 4) # Parallel Coords for Graph 1
+ggparcoord(qt2, columns = 1: ncol(qt2), groupColumn = 4) # Parallel Coords for Graph 2
+
+
