f9d4da5a3bf5e55e4b73edc1a506e8fde0fd6281
AtrayeeNeog <44379864+AtrayeeNeog@users.noreply.github.com>
2020-05-11 22:48:09 +0200

    Update Comparison1.R

--- RCodes_Atrayee/Comparison1.R
+++ RCodes_Atrayee/Comparison1.R
@@ -23,86 +23,19 @@ qt1_edgelist <- qt1_network
 qt1_graph <- graph.data.frame(qt1_edgelist, directed = TRUE)
 
 #Visualisations:
-#Template Network: First Try
-
-#Layout Options
-set.seed(3952)  # set seed to make the layout reproducible
-layout1 <- layout.fruchterman.reingold(dt_graph) #Creating a layout object to tell iGraph what layout I want
-
-#Node or Vertex Options: Color
-V(dt_graph)$color <- "grey"
-V(dt_graph)[degree(dt_graph, mode="in")>20]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
-
-#Edge Options: Siz
-E(dt_graph)$color <- "grey"
-
-#Plotting 
-plot(dt_graph)
-
-#Template Network: Second Try
-
-#Let work on get the nodes better first by making the sizing proportional to the network measure we care about, in this case degree
-V(dt_graph)$size=degree(dt_graph, mode = "in")/5 #because we have wide range, I am dividing by 5 to keep the high in-degree nodes from overshadowing everything else.
-
-#Node or Vertex Options: Color
-V(dt_graph)$color <- "grey"
-V(dt_graph)[degree(dt_graph, mode="in")>8]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
-
-#Edge Options: Siz
-E(dt_graph)$color <- "grey"
-
-#Plotting 
-plot(dt_graph)
 
 
-#Template Network: Third Try
-#Removing Arrows
-
-#Layout Options
-set.seed(3952)  # set seed to make the layout reproducible
-layout1 <- layout.fruchterman.reingold(dt_graph) #Creating a layout object to tell iGraph what layout I want
-
-#Node or Vetex Options: Size and Color
-V(dt_graph)$color <- "grey"
-V(dt_graph)[degree(dt_graph, mode="in")>8]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
-V(dt_graph)$size=degree(dt_graph, mode = "in")/5 #because we have wide range, I am dividing by 5 to keep the high in-degree nodes from overshadowing everything else.
-
-#Edge Options: Color
-E(dt_graph)$color <- "grey"
-
-#Plotting, Now Specifying an arrow size and getting rid of arrow heads
-#We are letting the color and the size of the node indicate the directed nature of the graph
-plot(dt_graph, edge.arrow.size=0.25,edge.arrow.mode = "-")
-
-#Template Network: Fourth Try
+#Template Network:
 
 #Removing Self-Loops (Repondents Nominating Themselves)
 dt_graph2<-simplify(dt_graph, remove.multiple=TRUE, remove.loops=TRUE)
-
-#Layout Options
-set.seed(3952)  # set seed to make the layout reproducible
-layout1 <- layout.fruchterman.reingold(dt_graph2) #Creating a layout object to tell iGraph what layout I want
-
-#Node or Vetex Options: Size and Color
-V(dt_graph2)$color <- "grey"
-V(dt_graph2)[degree(dt_graph, mode="in")>8]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
-V(dt_graph2)$size=degree(dt_graph, mode = "in")/5 #because we have wide range, I am dividing by 5 to keep the high in-degree nodes from overshadowing everything else.
-
-#Edge Options: Color
-E(dt_graph2)$color <- "grey"
-
-#Plotting, Now Specifying an arrow size and getting rid of arrow heads
-#We are letting the color and the size of the node indicate the directed nature of the graph
-plot(dt_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-")
-
-#Template Network: Fifth Try
 
 #Import the sample_attributes
 Template_Attributes=data.table::fread(here::here("data", "CGCS-Template-NodeTypes.csv"))
 
 #Layout Options
 set.seed(3952)  # set seed to make the layout reproducible
-layout1 <- layout.fruchterman.reingold(dt_graph2) #Creating a layout object to tell iGraph what layout I want
+layout1 <- layout.fruchterman.reingold(dt_graph2, niter=5000) #Creating a layout object to tell iGraph what layout I want
 
 #Node or Vetex Options: Size and Color
 V(dt_graph2)$color <- "grey"
@@ -126,3 +59,6 @@ E(dt_graph2)$color <- "grey"
 #Plotting, Now Specifying an arrow size and getting rid of arrow heads
 #We are letting the color and the size of the node indicate the directed nature of the graph
 plot(dt_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-")
+
+
+
