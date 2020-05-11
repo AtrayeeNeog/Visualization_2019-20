9a808a8d8d37dd57471f9a62647c32c3030b754c
AtrayeeNeog <44379864+AtrayeeNeog@users.noreply.github.com>
2020-05-11 23:18:06 +0200

    Update Comparison1.R

--- RCodes_Atrayee/Comparison1.R
+++ RCodes_Atrayee/Comparison1.R
@@ -58,7 +58,80 @@ E(dt_graph2)$color <- "grey"
 
 #Plotting, Now Specifying an arrow size and getting rid of arrow heads
 #We are letting the color and the size of the node indicate the directed nature of the graph
-plot(dt_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-")
+plot(dt_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-", vertex.label = NA)
+
+# Graph1 Network:
+
+#Removing Self-Loops (Repondents Nominating Themselves)
+qt1_graph2<-simplify(qt1_graph, remove.multiple=TRUE, remove.loops=TRUE)
+
+#Import the sample_attributes
+Graph1_Attributes=data.table::fread(here::here("data", "CGCS-GraphData-NodeTypes.csv"))
+
+#Layout Options
+set.seed(3952)  # set seed to make the layout reproducible
+layout1 <- layout.fruchterman.reingold(qt1_graph2, niter=1000) #Creating a layout object to tell iGraph what layout I want
+
+#Node or Vetex Options: Size and Color
+V(qt1_graph2)$color <- "grey"
+V(qt1_graph2)[degree(qt1_graph, mode="in")>8]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
+V(qt1_graph2)$size=degree(qt1_graph, mode = "in")/3 #because we have wide range, I am dividing by 5 to keep the high in-degree nodes from overshadowing everything else.
+
+#NodeType	Description
+#  1	      Person
+#  2	  Product category
+#  3	      Document
+#  4	  Financial category
+#  5	      Country
+
+V(qt1_graph2)$color <- ifelse(Template_Attributes[V(qt1_graph2), 2] == 1, "blue", 
+                             ifelse(Template_Attributes[V(qt1_graph2), 2] == 4, "red",
+                                    ifelse(Template_Attributes[V(qt1_graph2), 2] == 5, "green", "orange")))
+
+#Edge Options: Color
+E(qt1_graph2)$color <- "grey"
+
+#Plotting, Now Specifying an arrow size and getting rid of arrow heads
+#We are letting the color and the size of the node indicate the directed nature of the graph
+plot(qt1_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-", vertex.label = NA)
+
+# Graph1 Network: Kamadi Kawai
+
+#Removing Self-Loops (Repondents Nominating Themselves)
+qt1_graph2<-simplify(qt1_graph, remove.multiple=TRUE, remove.loops=TRUE)
+
+#Import the sample_attributes
+Graph1_Attributes=data.table::fread(here::here("data", "CGCS-GraphData-NodeTypes.csv"))
+
+#Layout Options
+set.seed(3952)  # set seed to make the layout reproducible
+layout1 <- layout_with_kk(qt1_graph2) #Creating a layout object to tell iGraph what layout I want
+
+#Node or Vetex Options: Size and Color
+V(qt1_graph2)$color <- "grey"
+V(qt1_graph2)[degree(qt1_graph, mode="in")>8]$color <- "yellow"  #Destinguishing High Degree Nodes as yellow
+V(qt1_graph2)$size=degree(qt1_graph, mode = "in")/3 #because we have wide range, I am dividing by 5 to keep the high in-degree nodes from overshadowing everything else.
+
+#NodeType	Description
+#  1	      Person
+#  2	  Product category
+#  3	      Document
+#  4	  Financial category
+#  5	      Country
+
+V(qt1_graph2)$color <- ifelse(Template_Attributes[V(qt1_graph2), 2] == 1, "blue", 
+                              ifelse(Template_Attributes[V(qt1_graph2), 2] == 4, "red",
+                                     ifelse(Template_Attributes[V(qt1_graph2), 2] == 5, "green", "orange")))
+
+#Edge Options: Color
+E(qt1_graph2)$color <- "grey"
+
+#Plotting, Now Specifying an arrow size and getting rid of arrow heads
+#We are letting the color and the size of the node indicate the directed nature of the graph
+plot(qt1_graph2, edge.arrow.size=0.25,edge.arrow.mode = "-", vertex.label = NA)
+
+
+
 
 
 
